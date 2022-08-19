a5893d3e86c8cf0c9cc1c5cdf295724906e10a5c
uzurpastor-lena <shevchenko.andrey.uzurpastor@gmail.com>
2022-08-19 10:45:23 +0300

    Configurate project add:         * devise         * aws s3         * active_admin         * rubocop         * rspec

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,21 @@
-require_relative "boot"
+# frozen_string_literal: true
 
-require "rails/all"
+require_relative 'boot'
+
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module CodicaTest
+module HealthWave
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
-    #
+    config.autoload_paths += %W[#{Rails.root}/app/models/account]
+
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
