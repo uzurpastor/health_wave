913d3a3e07f92e92e6cbc08c745c709002d6b035
uzurpastor-lena <shevchenko.andrey.uzurpastor@gmail.com>
2022-08-20 14:43:28 +0300

    devel: add attached file to user model

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -31,7 +31,7 @@ module Users
 
     # If you have extra params to permit, append them to the sanitizer.
     def configure_account_update_params
-      devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:attribute, :avatar])
     end
 
     # The path used after sign up.
