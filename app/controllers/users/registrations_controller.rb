e268d7ed39bcc3fa8fe4da8925f6524d1ee15e22
uzurpastor-lena <shevchenko.andrey.uzurpastor@gmail.com>
2022-08-20 14:30:24 +0300

    Revert "devel: add attached file to user model"
    This reverts commit 3bce56d9c96a795f094d136de6216d4ec2367a2b.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -31,7 +31,7 @@ module Users
 
     # If you have extra params to permit, append them to the sanitizer.
     def configure_account_update_params
-      devise_parameter_sanitizer.permit(:account_update, keys: [:attribute, :avatar])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
     end
 
     # The path used after sign up.
