db7bb3f547c4f38a85f1b15c3d419488631dd10b
uzurpastor-lena <shevchenko.andrey.uzurpastor@gmail.com>
2022-08-21 13:03:59 +0300

    devel: connect cancancan to project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,15 @@
 class ApplicationController < ActionController::Base
   devise_group :account, contains: %i[user doctor]
 
-  before_action :authenticate_account!
+  before_action :authenticate_account!, :current_ability
+  # load_and_authorize_resource
+
+  def current_ability
+  if user_signed_in?
+    @current_ability ||= Ability.new(current_user)
+  elsif doctor_signed_in?
+    @current_ability ||= Ability.new(current_doctor)
+  end
+end
+
 end
