9463ec37d8918c3cb9304d7193490ed7b3a29b2e
uzurpastor-lena <shevchenko.andrey.uzurpastor@gmail.com>
2022-08-22 14:00:02 +0300

    devel: add validations to reception model

--- app/controllers/users/receptions_controller.rb
+++ app/controllers/users/receptions_controller.rb
@@ -1,5 +1,11 @@
 module Users
   class ReceptionsController < ReceptionsController
+    before_action :collect_doctors, only: [:new, :create]
+
+    def show 
+      super
+    end
+    
     def index
       @receptions = current_user.receptions
       render 'users/receptions/index'    
@@ -7,24 +13,27 @@ module Users
 
     def new
       @reception = Reception.new
-      @doctors = Doctor.all.collect { |p| [ p.name, p.id ] } 
+      render 'users/receptions/new'
     end
 
     def create
-      reception = current_user.receptions.new( receptions_create_params )
-      if reception.save
+      @reception = current_user.receptions.new( receptions_create_params )
+      if @reception.save
         redirect_to users_index_receptions_path,
           flash: { success: "wait for setting time" }
       else
-        redirect_to users_new_reception_path,
-          flash: { danger: "some problems" }
+        render 'users/receptions/new'
       end
     end
 
     protected
 
+    def collect_doctors
+      @doctors= Doctor.all.collect { |p| [ p.name, p.id ] } 
+    end
+
     def receptions_create_params
-      params.require(:reception).permit(:doctor_id, :description).merge status: 'considering'
+      params.require(:reception).permit(:doctor_id, :description).merge status: :considering
     end
   end
 end
--- app/models/reception.rb
+++ app/models/reception.rb
@@ -3,10 +3,20 @@
 class Reception < ApplicationRecord
   belongs_to :user
   belongs_to :doctor
+
   enum :status, {
     considering: 'considering',
     waiting: 'waiting',
     reply: 'reply'
   }
+
+  validates_presence_of :description, :user_id, :doctor_id
+  validate :number_of_doctors_reception_less_then_10
+    
+  private def number_of_doctors_reception_less_then_10
+    return true  if self.doctor_id.blank?
+    doctor = Doctor.find self.doctor_id
+    receptions = doctor.receptions.select {|r| !r.reply?}
+    errors.add(:doctor_id, "cannot have more than 10 patients at the same time ") unless receptions.count > 10
+  end
 end
-# enum be: considering, waiting, reply
