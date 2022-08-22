1ae9d5e4995ac639dfc8b13e4be3b012ff872a53
uzurpastor-lena <shevchenko.andrey.uzurpastor@gmail.com>
2022-08-22 11:44:19 +0300

    devel: made that doctors can set the time of the meeting

--- app/controllers/doctors/receptions_controller.rb
+++ app/controllers/doctors/receptions_controller.rb
@@ -10,10 +10,18 @@ module Doctors
     end
 
     def edit_time
+      @reception = Reception.find params[:id]
       render 'doctors/receptions/edit_time'
     end
     def update_time
-
+      reception = Reception.find params[:id]
+      if reception.update(receptions_time_update_params)
+        redirect_to doctors_index_reception_path,
+          flash: {success: 'Success set time'}
+      else
+        redirect_to doctors_new_time_receptions_path,
+          flash: {danger: 'some problems'}
+      end
     end
 
     def edit_response
@@ -21,6 +29,12 @@ module Doctors
     end
     def update_response
       
+    end
+
+    protected
+
+    def receptions_time_update_params
+      params.require(:reception).permit(:time).merge status: :waiting      
     end
   end
 end
