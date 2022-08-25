fa24ddb926abb717bfa5ce4a762790aa64317513
uzurpastor <shevchenko.andrey.uzurpastor@gmail.com>
2022-08-25 15:54:44 +0300

    devel: rebuild controllers and views for using category model

--- app/controllers/doctors/displays_controller.rb
+++ app/controllers/doctors/displays_controller.rb
@@ -3,7 +3,8 @@
 module Doctors
   class DisplaysController < ApplicationController
     def show
-      @doctor = Doctor.select(:id, :email, :name, :category).find(params[:id])
+      @doctor = Doctor.select(:id, :email, :name, :category_id).find(params[:id])
+      @category = Category.find @doctor.category_id
       render 'doctors/displays/show'
     end
   end
