d0d538c2cc7e8114c1ca71152395eb60dcce999a
uzurpastor <shevchenko.andrey.uzurpastor@gmail.com>
2022-08-26 12:15:36 +0300

    beautify

--- app/controllers/doctors/receptions_controller.rb
+++ app/controllers/doctors/receptions_controller.rb
@@ -39,50 +39,53 @@ module Doctors
 
     protected
 
-    ## need to create servece for safe
-    def not_safety?
-      case params[:action].humanize.split(' ').first
-      when 'edit'
-        safe_edit?
-      when 'update'
-        safe_update
-      end
-    end
-    def safe_edit
-      action = params[:action].to_s
-      unless can? action, @reception 
-        redirect_to doctors_index_reception_path,
-                    flash: { danger: 'You cannot do this' } and return false
-      end
-      return true
-    end
-    def safe_update
-      action = params[:action].to_s
-      unless can? action, @reception 
-        Rails.logger.error { 
-          "#{params.inspect}" +
-          "#=> someone is trying to illegally set invalid data " }
-        return false
-      end
-      return true
-    end
-    def check_safety
-      self.return if not_safety?
-    end
-    ## end block for safe
+
+    ## START Safety service
+                          def not_safety?
+                            case params[:action].humanize.split(' ').first
+                            when 'edit'
+                              safe_edit?
+                            when 'update'
+                              safe_update
+                            end
+                          end
+                          def safe_edit
+                            action = params[:action].to_s
+                            unless can? action, @reception 
+                              redirect_to doctors_index_reception_path,
+                                          flash: { danger: 'You cannot do this' } and return false
+                            end
+                            return true
+                          end
+                          def safe_update
+                            action = params[:action].to_s
+                            unless can? action, @reception 
+                              Rails.logger.error { 
+                                "#{params.inspect}" +
+                                "#=> someone is trying to illegally set invalid data " }
+                              return false
+                            end
+                            return true
+                          end
+                          def check_safety
+                            self.return if not_safety?
+                          end
+    ## END
+
+
     def reception_for_current_doctor_by status
       current_doctor
         .receptions.where('status = ?', status)
         .select(:id, :description, :user_id, :status, :time)
     end
 
-    def set_reception
-      @reception = Reception.find params[:id]
-    end
-
     def status_param
       return 'considering' if params.permit(:status).blank?
       params[:status]
+    end
+
+    def set_reception
+      @reception = Reception.find params[:id]
     end
 
     def receptions_time_update_params
